515098a248683cc0579a340c9aa3f590f0b87ea5
Forrest Widmer <fwid77@gmail.com>
2013-10-09 16:41:11 -0700

    First commit and README update

---
+++ README.md
@@ -0,0 +1,3 @@
+**_A Reddit-style application to demonstrate foundation programming and web development skills._**
+
+Made with me mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = 'f876cc2f0353956870fd9aae7a0990c252eda86e0028c4d6e27f6c8d439888bdad581adba3d5b1fa192f745461314e719d5dd87245b92caf74668ce564933450'
