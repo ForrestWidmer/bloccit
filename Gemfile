c06b0ce2b6fec3ac0af18dc9b47568ff51f49fb9
Forrest Widmer <fwid77@gmail.com>
2013-11-12 17:28:05 -0700

    Implemented Facebook Authentication.

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'fog'
 gem 'figaro'
 gem 'unf'
 gem 'will_paginate', '~> 3.0'
+gem 'omniauth-facebook'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Bloccit::Application.routes.draw do
 
   get "comments/create"
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   
   resources :topics do
