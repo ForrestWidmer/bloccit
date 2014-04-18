8f0817546bbcdd6974bd1c5586f2fd0e912d6ffe
ForrestWidmer <forrest.widmer@gmail.com>
2014-04-18 10:57:09 -0700

    Created topics partial to display users topics on their public page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.visible_to(current_user)
+    @topics = @user.topics.visible_to(current_user)
   end
 
   def index
